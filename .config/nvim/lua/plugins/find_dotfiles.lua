5cc517e19495a04bb5cfe173a857bec9f8d14e63
Khang Hoang <khang@dropbox.com>
2021-10-04 18:59:06 +0000

    Use git_files instead of find_files

--- .config/nvim/lua/plugins/find_dotfiles.lua
+++ .config/nvim/lua/plugins/find_dotfiles.lua
@@ -1,6 +1,6 @@
 local M = {}
 M.find_dotfiles = function()
-    require("telescope.builtin").find_files({
+    require("telescope.builtin").git_files({
         prompt_title = "< VimRC >",
         -- cwd = "~/.config/nvim",
         cwd = vim.env.DOTFILES,
