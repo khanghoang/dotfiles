d63e645483e3b4ca25b0cc33f7e12af9b1678680
Khang Hoang <hoangtrieukhang@gmail.com>
2021-10-11 03:13:51 -0700

    Config gitsign

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -60,9 +60,31 @@ return require('packer').startup(function()
       'nvim-lua/plenary.nvim'
     },
     config = function()
-      require('gitsigns').setup()
-  end
-}
+      if not packer_plugins['plenary.nvim'].loaded then
+        vim.cmd [[packadd plenary.nvim]]
+      end
+      require('gitsigns').setup {
+        keymaps = {
+          -- Default keymap options
+          noremap = true,
+          buffer = true,
+
+          ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+          ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+
+          ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+          ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+          ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+          ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+          ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+
+          -- Text objects
+          ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
+          ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+        },
+      }
+    end
+  }
 
   -- Language support
   use {
