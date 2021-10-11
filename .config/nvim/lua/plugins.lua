4e549b1fdf9caee40cf4d106e39dbe3b67cf3b9a
Khang Hoang <khang@dropbox.com>
2021-10-11 01:23:22 +0000

    Indent line

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -33,7 +33,7 @@ return require('packer').startup(function()
 
   -- Language support
   use 'dense-analysis/ale'
-  -- use 'tree-sitter/tree-sitter-typescript'
+  use 'tree-sitter/tree-sitter-typescript'
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use 'nvim-treesitter/nvim-treesitter-refactor'
   use 'nvim-treesitter/playground'
@@ -50,7 +50,6 @@ return require('packer').startup(function()
   use 'windwp/nvim-autopairs'
   use 'tpope/vim-surround'
   use 'norcalli/nvim-colorizer.lua'
-  use 'lukas-reineke/indent-blankline.nvim'
   use 'simrat39/symbols-outline.nvim'
   use 'christoomey/vim-tmux-navigator'
   use 'tomtom/tcomment_vim'
@@ -75,9 +74,97 @@ return require('packer').startup(function()
     branch = 'main',
     -- event = { 'VimEnter' },
     config = function()
-      -- require('plugins.lightline')
+       require('plugins.lightline')
     end,
     requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'BufEnter',
+    branch = 'master',
+    config = function()
+      vim.g.indent_blankline_char = "│"
+      vim.g.indent_blankline_show_first_indent_level = true
+      vim.g.indent_blankline_filetype_exclude = {
+        "startify",
+        "dashboard",
+        "dotooagenda",
+        "log",
+        "fugitive",
+        "gitcommit",
+        "packer",
+        "vimwiki",
+        "markdown",
+        "json",
+        "txt",
+        "vista",
+        "help",
+        "todoist",
+        "NvimTree",
+        "peekaboo",
+        "git",
+        "TelescopePrompt",
+        "undotree",
+        "flutterToolsOutline",
+        "" -- for all buffers without a file type
+      }
+      vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
+      vim.g.indent_blankline_show_trailing_blankline_indent = false
+      vim.g.indent_blankline_show_current_context = true
+      vim.g.indent_blankline_context_patterns = {
+        "abstract_class_declaration", "abstract_method_signature",
+        "accessibility_modifier", "ambient_declaration", "arguments", "array",
+        "array_pattern", "array_type", "arrow_function", "as_expression",
+        "asserts", "assignment_expression", "assignment_pattern",
+        "augmented_assignment_expression", "await_expression",
+        "binary_expression", "break_statement", "call_expression",
+        "call_signature", "catch_clause", "class", "class_body",
+        "class_declaration", "class_heritage", "computed_property_name",
+        "conditional_type", "constraint", "construct_signature",
+        "constructor_type", "continue_statement", "debugger_statement",
+        "declaration", "decorator", "default_type", "do_statement",
+        "else_clause", "empty_statement", "enum_assignment", "enum_body",
+        "enum_declaration", "existential_type", "export_clause",
+        "export_specifier", "export_statement", "expression",
+        "expression_statement", "extends_clause", "finally_clause",
+        "flow_maybe_type", "for_in_statement", "for_statement",
+        "formal_parameters", "function", "function_declaration",
+        "function_signature", "function_type", "generator_function",
+        "generator_function_declaration", "generic_type", "if_statement",
+        "implements_clause", "import", "import_alias", "import_clause",
+        "import_require_clause", "import_specifier", "import_statement",
+        "index_signature", "index_type_query", "infer_type",
+        "interface_declaration", "internal_module", "intersection_type",
+        "jsx_attribute", "jsx_closing_element", "jsx_element", "jsx_expression",
+        "jsx_fragment", "jsx_namespace_name", "jsx_opening_element",
+        "jsx_self_closing_element", "labeled_statement", "lexical_declaration",
+        "literal_type", "lookup_type", "mapped_type_clause",
+        "member_expression", "meta_property", "method_definition",
+        "method_signature", "module", "named_imports", "namespace_import",
+        "nested_identifier", "nested_type_identifier", "new_expression",
+        "non_null_expression", "object", "object_assignment_pattern",
+        "object_pattern", "object_type", "omitting_type_annotation",
+        "opting_type_annotation", "optional_parameter", "optional_type", "pair",
+        "pair_pattern", "parenthesized_expression", "parenthesized_type",
+        "pattern", "predefined_type", "primary_expression", "program",
+        "property_signature", "public_field_definition", "readonly_type",
+        "regex", "required_parameter", "rest_pattern", "rest_type",
+        "return_statement", "sequence_expression", "spread_element",
+        "statement", "statement_block", "string", "subscript_expression",
+        "switch_body", "switch_case", "switch_default", "switch_statement",
+        "template_string", "template_substitution", "ternary_expression",
+        "throw_statement", "try_statement", "tuple_type",
+        "type_alias_declaration", "type_annotation", "type_arguments",
+        "type_parameter", "type_parameters", "type_predicate",
+        "type_predicate_annotation", "type_query", "unary_expression",
+        "union_type", "update_expression", "variable_declaration",
+        "variable_declarator", "while_statement", "with_statement",
+        "yield_expression"
+      }
+      -- because lazy load indent-blankline so need readd this autocmd
+      vim.cmd('autocmd CursorMoved * IndentBlanklineRefresh')
+    end,
   }
 
 end)
