34daffe1a8e8933bc1e73dc215d10a72b70ec65a
Khang Hoang <hoangtrieukhang@gmail.com>
2022-04-23 01:04:06 -0700

    new nvim-cmp config for neovim 0.7

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -102,13 +102,20 @@ return require('packer').startup(function()
             vim.fn["vsnip#anonymous"](args.body)
           end,
         },
-        mapping = {
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-k'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        },
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end
+        }),
         sources = {
           { name = 'nvim_lsp' },
           -- For vsnip user.
