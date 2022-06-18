3294146b3ea1b68db46ffa3be7b66526ad6fddac
Khang Hoang <hoangtrieukhang@gmail.com>
2022-06-17 22:52:33 -0700

    custom status line

--- .config/nvim/lua/plugins/lightline.lua
+++ .config/nvim/lua/plugins/lightline.lua
@@ -2,38 +2,26 @@ local gl = require('galaxyline')
 local colors = require('galaxyline.themes.colors').default
 local condition = require('galaxyline.condition')
 local gls = gl.section
-gl.short_line_list = {'packer'}
 
-gls.right[1] = {
-  GitBranch = {
-    provider = 'GitBranch',
-    condition = condition.check_git_workspace,
-    highlight = {colors.violet,colors.bg,'bold'},
-  }
-}
-
-gls.left[2] = {
-  FileName = {
-    provider = 'FileName',
-    condition = condition.buffer_not_empty,
-    highlight = {colors.magenta,colors.bg,'bold'}
-  }
-}
+-- Default colors and providers
+-- https://github.com/glepnir/galaxyline.nvim#default-provider-groups
 
 gls.left[3] = {
   DiagnosticError = {
     condition = condition.buffer_not_empty,
     provider = 'DiagnosticError',
-    icon = ' E:',
-    highlight = {colors.red,colors.bg}
+    separator = '',
+    highlight = {colors.red,colors.bg},
+    separator_highlight = {'NONE',colors.bg},
   }
 }
 gls.left[4] = {
   DiagnosticWarn = {
     condition = condition.buffer_not_empty,
     provider = 'DiagnosticWarn',
-    icon = ' W:',
+    separator = '',
     highlight = {colors.yellow,colors.bg},
+    separator_highlight = {'NONE',colors.bg},
   }
 }
 
@@ -41,8 +29,9 @@ gls.left[5] = {
   DiagnosticHint = {
     condition = condition.buffer_not_empty,
     provider = 'DiagnosticHint',
-    icon = ' H:',
+    separator = '',
     highlight = {colors.cyan,colors.bg},
+    separator_highlight = {'NONE',colors.bg},
   }
 }
 
@@ -50,8 +39,9 @@ gls.left[6] = {
   DiagnosticInfo = {
     condition = condition.buffer_not_empty,
     provider = 'DiagnosticInfo',
-    icon = ' I:',
+    icon = '',
     highlight = {colors.blue,colors.bg},
+    separator_highlight = {'NONE',colors.bg},
   }
 }
 
@@ -60,14 +50,15 @@ gls.left[7] = {
     provider = 'GetLspClient',
     condition = condition.buffer_not_empty,
     icon = ' LSP:',
-    highlight = {colors.cyan,colors.bg,'bold'}
+    highlight = {colors.cyan,colors.bg},
+    separator_highlight = {'NONE',colors.bg},
+    separator = " ",
   }
 }
 
 gls.right[1] = {
-  FileEncode = {
-    provider = 'FileEncode',
-    condition = condition.hide_in_width,
+  LinePercent = {
+    provider = 'LinePercent',
     separator = ' ',
     separator_highlight = {'NONE',colors.bg},
     highlight = {colors.green,colors.bg,'bold'}
@@ -75,60 +66,62 @@ gls.right[1] = {
 }
 
 gls.right[2] = {
-  FileFormat = {
-    provider = 'FileFormat',
-    condition = condition.hide_in_width,
-    separator = ' ',
-    separator_highlight = {'NONE',colors.bg},
-    highlight = {colors.green,colors.bg,'bold'}
-  }
-}
-
-gls.right[5] = {
   DiffAdd = {
     provider = 'DiffAdd',
-    condition = condition.hide_in_width,
     icon = '+',
     highlight = {colors.green,colors.bg},
   }
 }
-gls.right[6] = {
+gls.right[3] = {
   DiffModified = {
     provider = 'DiffModified',
-    condition = condition.hide_in_width,
     icon = '~',
     highlight = {colors.orange,colors.bg},
   }
 }
-gls.right[7] = {
+gls.right[4] = {
   DiffRemove = {
     provider = 'DiffRemove',
-    condition = condition.hide_in_width,
     icon = '-',
     highlight = {colors.red,colors.bg},
   }
 }
 
-gls.short_line_left[1] = {
-  BufferType = {
-    provider = 'FileTypeName',
-    separator = ' ',
+gls.mid[1] = {
+  GitBranch = {
+    provider = 'GitBranch',
+    condition = condition.check_git_workspace,
+    highlight = {"#6e6763", "#352f2d"},
     separator_highlight = {'NONE',colors.bg},
-    highlight = {colors.blue,colors.bg,'bold'}
   }
 }
 
-gls.short_line_left[2] = {
-  SFileName = {
-    provider =  'SFileName',
+gls.mid[2] = {
+  FileName = {
+    provider = 'FilePath',
     condition = condition.buffer_not_empty,
-    highlight = {colors.fg,colors.bg,'bold'}
+    highlight = {"#b4bdc3", "#352f2d"},
+    separator = ' - ',
+    separator_highlight = {'NONE', "#352f2d"},
   }
 }
 
-gls.short_line_right[1] = {
-  BufferIcon = {
-    provider= 'BufferIcon',
-    highlight = {colors.fg,colors.bg}
-  }
-}
+vim.cmd [[hi StatusLine guibg=#352f2d guifg=#352f2d]]
+
+local M = {}
+
+M.reload = function ()
+  local gl = require('galaxyline')
+  local gls = gl.section
+
+  gls.right = {}
+  gls.left= {}
+
+  require("plenary.reload").reload_module("NTBBloodbath/galaxyline.nvim")
+  require("plenary.reload").reload_module("plugins.lightline")
+  require("plugins.lightline")
+
+  print('reloaded')
+end
+
+return M
