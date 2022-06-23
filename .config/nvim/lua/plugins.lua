e53b9758e96dbbb502c1f9ea000887a870447eda
Khang Hoang <hoangtrieukhang@gmail.com>
2022-06-23 13:55:31 -0700

    config for sql ui

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -915,7 +915,24 @@ return require('packer').startup(function()
   -- SQL
   -- {{{
   use "tpope/vim-dadbod"
-  use { "kristijanhusak/vim-dadbod-completion" }
+  use {
+    "kristijanhusak/vim-dadbod-completion",
+    config = function ()
+      vim.api.cmd [[
+        autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+        " Source is automatically added, you just need to include it in the chain complete list
+        let g:completion_chain_complete_list = {
+            \   'sql': [
+            \    {'complete_items': ['vim-dadbod-completion']},
+            \   ],
+            \ }
+        " Make sure `substring` is part of this list. Other items are optional for this completion source
+        let g:completion_matching_strategy_list = ['exact', 'substring']
+        " Useful if there's a lot of camel case items
+        let g:completion_matching_ignore_case = 1
+      ]]
+    end
+  }
   use { "kristijanhusak/vim-dadbod-ui" }
   -- }}}
 
