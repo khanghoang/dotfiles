3f403aad7c21cfc9e2b3a66488c95661050914a4
Hoang Trieu, Khang <khoangtrieu@paypal.com>
2022-08-01 13:13:49 -0700

    writing custom script

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -53,17 +53,38 @@ M.openNearestFile = function (file)
   -- package.json not found
 end
 
-vim.keymap.set('n', '<leader>rb', ":lua require('plugins/lightline').openNearestFile()<CR>")
+vim.keymap.set('n', '<leader>rb', ":lua require('plugins/utils').openNearestFile()<CR>")
 
 vim.api.nvim_create_user_command("OpenNearestPackageJSON", function(opts)
-  require('plugins/lightline').openNearestPackageJSON()
+  require('plugins/utils').openNearestPackageJSON()
 end, {
     desc = "Install one or more packages.",
     nargs = "+",
     complete = "custom,v:lua.mason_completion.available_package_completion",
   })
 
-vim.api.nvim_set_keymap('n', '<leader><leader>r', ":lua require('plugins.lightline').reload()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader><leader>r', ":lua require('plugins.utils').reload()<CR>", { noremap = true })
+
+M.get_visual_selection = function ()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
+
+M.test = function ()
+  local s = M.get_visual_selection()
+  vim.notify(s)
+end
+
+vim.api.nvim_set_keymap('v', '<leader>T', ":lua require('plugins.utils').test()<CR>", { noremap = true })
 
 M.reload = function ()
   require("plenary.reload").reload_module("plugins.utils")
