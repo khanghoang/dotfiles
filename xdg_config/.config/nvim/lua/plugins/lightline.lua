7416b8a61bb5354866ae42db0ca300b94057bc27
Khang Hoang <hoangtrieukhang@gmail.com>
2022-08-07 00:54:25 -0700

    finish the basic version of lsp's spinner

--- xdg_config/.config/nvim/lua/plugins/lightline.lua
+++ xdg_config/.config/nvim/lua/plugins/lightline.lua
@@ -1,59 +1,14 @@
 local gl = require('galaxyline')
 local colors = require('galaxyline.themes.colors').default
 local condition = require('galaxyline.condition')
+local log = require('plugins.log')
 local gls = gl.section
 local M = {}
 
-local get_notif_data = function (client_id)
-  local clients = {}
-  return clients[client_id]
-end
+-- enable log
+log.new({ level = "debug" }, true)
 
-vim.lsp.handlers["$/progress"] = function(_, result, ctx)
- local client_id = ctx.client_id
- local val = result.value
-
- if not val.kind then
-   return
- end
-
- local notif_data = get_notif_data(client_id, result.token)
-
- if val.kind == "begin" then
-   local message = format_message(val.message, val.percentage)
-
-   notif_data.notification = vim.notify(message, "info", {
-     title = format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
-     icon = spinner_frames[1],
-     timeout = false,
-     hide_from_history = false,
-   })
-
-   notif_data.spinner = 1
-   update_spinner(client_id, result.token)
- elseif val.kind == "report" and notif_data then
-   notif_data.notification = vim.notify(format_message(val.message, val.percentage), "info", {
-     replace = notif_data.notification,
-     hide_from_history = false,
-   })
- elseif val.kind == "end" and notif_data then
-   notif_data.notification =
-     vim.notify(val.message and format_message(val.message) or "Complete", "info", {
-       icon = "",
-       replace = notif_data.notification,
-       timeout = 3000,
-     })
-
-   notif_data.spinner = nil
- end
-end
-
-local lspIcon = require "plugins.icons".ui.ChevronRight
-
-local i = 1
-local time = 1
-local status = '';
-local spinner = {
+local indicator = {
     "⠋",
     "⠙",
     "⠹",
@@ -66,33 +21,98 @@ local spinner = {
     "⠏",
   }
 
-local timer = vim.loop.new_timer()
-timer:start(
-  200,
-  200,
-  vim.schedule_wrap(function()
-    i = i < 10 and i + 1 or 1
-    time = time + 1
-    status = ' '..spinner[i]..' '
-    if (time == 20) then
-      timer:stop()
+-- { client_id: spinner }
+local spinners = {}
+local make_spinner = function ()
+  local timer = vim.loop.new_timer()
+  local status = ''
+  local i = 1
+
+  return {
+    start = function ()
+      log.debug(
+        "Spinner starts",
+        {}
+      )
+      timer:start(
+        0, -- wait
+        100, -- every
+        vim.schedule_wrap(function()
+          i = i < 10 and i + 1 or 1
+          status = ' '..indicator[i]..' '
+        end)
+      )
+    end,
+    stop = function ()
       status = ''
+      timer:stop()
+    end,
+    get_status = function ()
+      return status
     end
-  end)
-)
+  }
+end
+
+
+local get_spinner_data = function (client_id)
+  if not spinners[client_id] then
+    spinners[client_id] = make_spinner()
+    log.debug('create spinner with id', { client_id = client_id, spinner = spinners[client_id] })
+    return spinners[client_id]
+  end
+
+  return spinners[client_id]
+end
+
+vim.lsp.handlers["$/progress"] = function(err, result, ctx)
+  -- See: https://microsoft.github.io/language-server-protocol/specifications/specification-current/#progress
+
+  log.debug(
+    "Received progress notification:",
+    { err = err, result = result, ctx = ctx }
+  )
+
+  local client_id = ctx.client_id
+  local val = result.value
+
+  if not val.kind then
+    return
+  end
+
+  local s = get_spinner_data(client_id)
+
+  log.debug(
+    "Get spinner",
+    { spinner = s }
+  )
+
+  if val.kind == "begin" then
+    s.start()
+  elseif val.kind == "report" and s then
+    -- do nothing, for now
+  elseif val.kind == "end" and s then
+    s.stop()
+  end
+end
+
+local lspIcon = require "plugins.icons".ui.ChevronRight
 
 local get_lsp_client = function (msg)
   msg = msg or ''
   local buf_ft = vim.api.nvim_buf_get_option(0,'filetype')
   local clients = vim.lsp.get_active_clients()
-  if next(clients) == nil then
-    return msg
-  end
+  -- if next(clients) == nil then
+  --   return msg
+  -- end
 
-  for _,client in ipairs(clients) do
+  -- ???
+  -- looks like all the pending/progressing lsp client get filtered out
+  for _, client in ipairs(clients) do
+    log.debug("client id", { client_id = client.id })
     local filetypes = client.config.filetypes
     if filetypes and vim.fn.index(filetypes,buf_ft) ~= -1 then
-      msg = msg..' '..client.name
+      local s = get_spinner_data(client.id)
+      msg = msg..' '..s.get_status()..client.name
     end
   end
   return msg
@@ -117,18 +137,19 @@ local function split(pString, pPattern)
    return Table
 end
 
-gls.left[3] = {
-  DiagnosticError = {
-    condition = condition.buffer_not_empty,
-    provider = function ()
-      return status
-    end,
-    icon = ' ',
-    separator = '',
-    highlight = {colors.yellow,colors.bg},
-    separator_highlight = {'NONE',colors.bg},
-  }
-}
+-- gls.left[3] = {
+--   DiagnosticError = {
+--     condition = condition.buffer_not_empty,
+--     provider = function ()
+--       return status
+--     end,
+--     icon = ' ',
+--     separator = '',
+--     highlight = {colors.yellow,colors.bg},
+--     separator_highlight = {'NONE',colors.bg},
+--   }
+-- }
+
 gls.left[4] = {
   DiagnosticWarn = {
     condition = condition.buffer_not_empty,
