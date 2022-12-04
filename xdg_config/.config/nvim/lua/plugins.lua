7b93c7901b0186fe8a621c77e440a526df254e46
Khang Hoang <hoangtrieukhang@gmail.com>
2022-12-04 14:32:36 -0800

    config neovim dap for nextjs

--- xdg_config/.config/nvim/lua/plugins.lua
+++ xdg_config/.config/nvim/lua/plugins.lua
@@ -810,11 +810,138 @@ require('packer').startup(function()
         }
       }
 
+      -- this one works with nextjs, the previous one doesn't
+      require('dap-vscode-js').setup({
+        node_path = 'node',
+        debugger_path = os.getenv('HOME') .. '/.DAP/vscode-js-debug',
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+      })
+
+      local exts = {
+        'javascript',
+        'typescript',
+        'javascriptreact',
+        'typescriptreact',
+        -- using pwa-chrome
+        'vue',
+        'svelte',
+      }
+
+      for i, ext in ipairs(exts) do
+        dap.configurations[ext] = {
+          -- this one works with nextjs
+          {
+            type = 'pwa-node',
+            request = 'attach',
+            name = 'Attach Program (pwa-node)',
+            cwd = vim.fn.getcwd(),
+            -- processId = require('dap.utils').pick_process,
+            skipFiles = { '<node_internals>/**' },
+            host = 'localhost',
+            port = 9229,
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch Current File (pwa-node)',
+            cwd = vim.fn.getcwd(),
+            args = { '${file}' },
+            sourceMaps = true,
+            protocol = 'inspector',
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch Current File (pwa-node with ts-node)',
+            cwd = vim.fn.getcwd(),
+            runtimeArgs = { '--loader', 'ts-node/esm' },
+            runtimeExecutable = 'node',
+            args = { '${file}' },
+            sourceMaps = true,
+            protocol = 'inspector',
+            skipFiles = { '<node_internals>/**', 'node_modules/**' },
+            resolveSourceMapLocations = {
+              "${workspaceFolder}/**",
+              "!**/node_modules/**",
+            },
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch Current File (pwa-node with deno)',
+            cwd = vim.fn.getcwd(),
+            runtimeArgs = { 'run', '--inspect-brk', '--allow-all', '${file}' },
+            runtimeExecutable = 'deno',
+            attachSimplePort = 9229,
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch Test Current File (pwa-node with jest)',
+            cwd = vim.fn.getcwd(),
+            runtimeArgs = { '${workspaceFolder}/node_modules/.bin/jest' },
+            runtimeExecutable = 'node',
+            args = { '${file}', '--coverage', 'false'},
+            rootPath = '${workspaceFolder}',
+            sourceMaps = true,
+            console = 'integratedTerminal',
+            internalConsoleOptions = 'neverOpen',
+            skipFiles = { '<node_internals>/**', 'node_modules/**' },
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch Test Current File (pwa-node with vitest)',
+            cwd = vim.fn.getcwd(),
+            program = '${workspaceFolder}/node_modules/vitest/vitest.mjs',
+            args = { '--inspect-brk', '--threads', 'false', 'run', '${file}' },
+            autoAttachChildProcesses = true,
+            smartStep = true,
+            console = 'integratedTerminal',
+            skipFiles = { '<node_internals>/**', 'node_modules/**' },
+          },
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch Test Current File (pwa-node with deno)',
+            cwd = vim.fn.getcwd(),
+            runtimeArgs = { 'test', '--inspect-brk', '--allow-all', '${file}' },
+            runtimeExecutable = 'deno',
+            attachSimplePort = 9229,
+          },
+          {
+            type = 'pwa-chrome',
+            request = 'attach',
+            name = 'Attach Program (pwa-chrome = { port: 9222 })',
+            program = '${file}',
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+            port = 9222,
+            webRoot = '${workspaceFolder}',
+          },
+          {
+            type = 'node2',
+            request = 'attach',
+            name = 'Attach Program (Node2)',
+            processId = require('dap.utils').pick_process,
+          },
+          {
+            type = 'node2',
+            request = 'attach',
+            name = 'Attach Program (Node2 with ts-node)',
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+            skipFiles = { '<node_internals>/**' },
+            port = 9229,
+          },
+        }
+      end
+
       vim.fn.sign_define('DapBreakpoint', {text = '●', texthl = 'GruvboxRed', linehl = '', numhl = ''})
 
       api.nvim_set_keymap('n', '<leader>dd', ":lua require('dap').continue()<CR>", {noremap = true})
       api.nvim_set_keymap('n', '<leader>dbp', ":lua require('dap').toggle_breakpoint()<CR>", {noremap = true})
-      api.nvim_set_keymap('n', '<leader>de', ":lua require('dap').close()<CR>", {noremap = true})
+      api.nvim_set_keymap('n', '<leader>de', ":lua require('dap').terminate()<CR>", {noremap = true})
       api.nvim_set_keymap('n', '<leader>dc', ":lua require('dap').continue()<CR>", {noremap = true})
       api.nvim_set_keymap('n', '<leader>dr', ":lua require('dap').repl.open({}, 'vsplit')<CR><C-w>la", {noremap = true})
 
@@ -831,7 +958,7 @@ require('packer').startup(function()
               'stacks',
               'watches',
             },
-            size = 80,
+            size = 30,
             position = 'left',
           },
           {
@@ -964,6 +1091,13 @@ require('packer').startup(function()
       -- because lazy load indent-blankline so need readd this autocmd
       vim.cmd('autocmd CursorMoved * IndentBlanklineRefresh')
     end,
+  }
+
+  use { "mxsdev/nvim-dap-vscode-js", requires = {"mfussenegger/nvim-dap"} }
+  use {
+    "microsoft/vscode-js-debug",
+    opt = true,
+    run = "npm install --legacy-peer-deps && npm run compile",
   }
 
   use {
