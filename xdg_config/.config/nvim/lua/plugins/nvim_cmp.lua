d1d780516e861328b4ea8defc33d94a58abd0019
Khang Hoang <hoangtrieukhang@gmail.com>
2023-04-13 21:36:35 -0700

    split nvim_cmp as separate file

---
+++ xdg_config/.config/nvim/lua/plugins/nvim_cmp.lua
@@ -0,0 +1,71 @@
+local cmp = require 'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- For `vsnip` user.
+      -- vim.fn["vsnip#anonymous"](args.body)
+      require 'luasnip'.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end
+  }),
+  sources = {
+    -- Copilot Source
+    { name = "copilot", group_index = 2 },
+    -- other sources
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'luasnip' },
+    { name = 'nvim_lsp' },
+    -- For vsnip user.
+    { name = 'path' },
+    { name = 'vsnip' },
+    { name = 'buffer' },
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      local LSP_KIND_ICONS = {
+        Text = '  ',
+        Method = '  ',
+        Function = '  ',
+        Constructor = '  ',
+        Field = '  ',
+        Variable = '  ',
+        Class = '  ',
+        Interface = '  ',
+        Module = '  ',
+        Property = '  ',
+        Unit = '  ',
+        Value = '  ',
+        Enum = '  ',
+        Keyword = '  ',
+        Snippet = '  ',
+        Color = '  ',
+        File = '  ',
+        Reference = '  ',
+        Folder = '  ',
+        EnumMember = '  ',
+        Constant = '  ',
+        Struct = '  ',
+        Event = '  ',
+        Operator = '  ',
+        TypeParameter = '  ',
+        Copilot = "",
+      }
+      vim_item.kind = string.format("%s %s", LSP_KIND_ICONS[vim_item.kind], vim_item.kind)
+      return vim_item
+    end
+  },
+})
