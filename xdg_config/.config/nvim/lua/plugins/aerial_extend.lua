d11d69cc56d13942973c6b3e8ca869ac1dd989dd
Khang Hoang <hoangtrieukhang@gmail.com>
2023-04-16 09:43:42 -0700

    refactor code

--- xdg_config/.config/nvim/lua/plugins/aerial_extend.lua
+++ xdg_config/.config/nvim/lua/plugins/aerial_extend.lua
@@ -1,28 +1,22 @@
+local fs_utils = require("plugins.fs_utils")
 local parsers = require("nvim-treesitter.parsers")
--- local query = require("nvim-treesitter.query")
-
--- local lua_code = [[
---   local function foo(x)
---     print(x)
---   end
--- ]]
 
 local bufnr = vim.api.nvim_create_buf(false, true)
--- split lua_code into lines
--- local lines = vim.split(lua_code, "\n")
-vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {
-  "def hello():",
-  "  print('Hello, world!')",
-})
-print(vim.inspect(bufnr))
-
--- vim.treesitter.highlighter.new(bufnr, "python"):update()
-
+vim.api.nvim_buf_set_lines(
+  bufnr,
+  0,
+  -1,
+  false,
+  vim.split(fs_utils.get_file_content("/Users/khanghoang/code/pytest-example/myapp/app.py"), "\n")
+)
 local parser = vim.treesitter.get_parser(bufnr, "python")
 local tree = parser:parse()[1]
 
 local query_string = [[
   (function_definition
+    name: (identifier) @name
+    )
+  (class_definition
     name: (identifier) @name
     )
 ]]
@@ -32,13 +26,8 @@ local query = vim.treesitter.query.parse("python", query_string)
 -- execute the query on the syntax tree
 local matches = query:iter_captures(tree:root(), bufnr)
 
--- create a table to store the function names
-local function_names = {}
-
 for id, node, metadata in matches do
   local name = query.captures[id] -- name of the capture in the query
   local q = require("vim.treesitter")
   print(vim.treesitter.get_node_text(node, bufnr))
 end
-
-print(vim.inspect(function_names))
--- xdg_config/.config/nvim/lua/plugins/fs_utils.lua
+++ xdg_config/.config/nvim/lua/plugins/fs_utils.lua
@@ -1,38 +1,49 @@
 --- Check if a file or directory exists in this path
 local function exists(file)
-	local ok, err, code = os.rename(file, file)
-	if not ok then
-		if code == 13 then
-			-- Permission denied, but it exists
-			return true
-		end
-	end
-	return ok, err
+  local ok, err, code = os.rename(file, file)
+  if not ok then
+    if code == 13 then
+      -- Permission denied, but it exists
+      return true
+    end
+  end
+  return ok, err
 end
 
 --- Check if a directory exists in this path
 local function is_dir(path)
-	-- "/" works on both Unix and Windows
-	return exists(path .. "/")
+  -- "/" works on both Unix and Windows
+  return exists(path .. "/")
 end
 
 local function mkdir(path)
-	os.execute("mkdir " .. path)
+  os.execute("mkdir " .. path)
 end
 
 local function write_file(path, content, mode)
-	local fp = io.open(path, mode)
-	-- parse object back to json
-	local str = string.format("%s", content)
-	if fp then
-		fp:write(str)
-		fp:close()
-	end
+  local fp = io.open(path, mode)
+  -- parse object back to json
+  local str = string.format("%s", content)
+  if fp then
+    fp:write(str)
+    fp:close()
+  end
+end
+
+local function get_file_content(file_path)
+  local f = io.open(file_path, "rb")
+  if not f then
+    return ""
+  end
+  local content = f:read("*all")
+  f:close()
+  return content
 end
 
 return {
-	exists = exists,
-	is_dir = is_dir,
-	mkdir = mkdir,
-	write_file = write_file,
+  exists = exists,
+  is_dir = is_dir,
+  mkdir = mkdir,
+  write_file = write_file,
+  get_file_content = get_file_content,
 }
