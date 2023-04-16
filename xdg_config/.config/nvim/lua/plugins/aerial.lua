f67e0e0ec85de6944f6566fec461750eb2860a5f
Khang Hoang <hoangtrieukhang@gmail.com>
2023-04-16 11:39:28 -0700

    format

--- xdg_config/.config/nvim/lua/plugins/aerial.lua
+++ xdg_config/.config/nvim/lua/plugins/aerial.lua
@@ -1,63 +1,63 @@
 require("aerial").setup({
-	nerd_font = false,
+  nerd_font = false,
 
-	filter_kind = {
-		"Class",
-		"Constructor",
-		-- "Constant",
-		-- "Enum",
-		"Function",
-		"Interface",
-		"Module",
-		"Method",
-		"Struct",
-	},
+  filter_kind = {
+    "Class",
+    "Constructor",
+    -- "Constant",
+    -- "Enum",
+    "Function",
+    "Interface",
+    "Module",
+    "Method",
+    "Struct",
+  },
 
-	icons = {
-		Text = "  ",
-		Method = "  ",
-		Function = "  ",
-		Constructor = "  ",
-		Field = "  ",
-		Variable = "  ",
-		Class = "  ",
-		Interface = "  ",
-		Module = "  ",
-		Property = "  ",
-		Unit = "  ",
-		Value = "  ",
-		Enum = "  ",
-		Keyword = "  ",
-		Snippet = "  ",
-		Color = "  ",
-		File = "  ",
-		Reference = "  ",
-		Folder = "  ",
-		EnumMember = "  ",
-		Constant = "  ",
-		Struct = "  ",
-		Event = "  ",
-		Operator = "  ",
-		TypeParameter = "  ",
-	},
+  icons = {
+    Text = "  ",
+    Method = "  ",
+    Function = "  ",
+    Constructor = "  ",
+    Field = "  ",
+    Variable = "  ",
+    Class = "  ",
+    Interface = "  ",
+    Module = "  ",
+    Property = "  ",
+    Unit = "  ",
+    Value = "  ",
+    Enum = "  ",
+    Keyword = "  ",
+    Snippet = "  ",
+    Color = "  ",
+    File = "  ",
+    Reference = "  ",
+    Folder = "  ",
+    EnumMember = "  ",
+    Constant = "  ",
+    Struct = "  ",
+    Event = "  ",
+    Operator = "  ",
+    TypeParameter = "  ",
+  },
 
-	-- Customize the characters used when show_guides = true
-	guides = {
-		-- When the child item has a sibling below it
-		mid_item = "├─",
-		-- When the child item is the last in the list
-		last_item = "└─",
-		-- When there are nested child guides to the right
-		nested_top = "│ ",
-		-- Raw indentation
-		whitespace = "  ",
-	},
+  -- Customize the characters used when show_guides = true
+  guides = {
+    -- When the child item has a sibling below it
+    mid_item = "├─",
+    -- When the child item is the last in the list
+    last_item = "└─",
+    -- When there are nested child guides to the right
+    nested_top = "│ ",
+    -- Raw indentation
+    whitespace = "  ",
+  },
 
-	backends = {
-		["_"] = { "treesitter" },
-		python = { "treesitter" },
-		lua = { "treesitter" },
-	},
+  backends = {
+    ["_"] = { "treesitter" },
+    python = { "treesitter" },
+    lua = { "treesitter" },
+  },
 })
 
 local map = vim.api.nvim_set_keymap
@@ -70,18 +70,18 @@ map("n", "do", ":Telescope aerial theme=ivy previewer=false winblend=10<CR>", op
 require("telescope").load_extension("aerial")
 
 require("telescope").setup({
-	extensions = {
-		aerial = {
-			-- Display symbols as <root>.<parent>.<symbol>
-			show_nesting = {
-				["_"] = false, -- This key will be the default
-				json = true, -- You can set the option for specific filetypes
-				yaml = true,
-			},
-			layout_config = {
-				vertical = { width = 0.5 },
-				-- other layout configuration here
-			},
-		},
-	},
+  extensions = {
+    aerial = {
+      -- Display symbols as <root>.<parent>.<symbol>
+      show_nesting = {
+        ["_"] = false, -- This key will be the default
+        json = true, -- You can set the option for specific filetypes
+        yaml = true,
+      },
+      layout_config = {
+        vertical = { width = 0.5 },
+        -- other layout configuration here
+      },
+    },
+  },
 })
--- xdg_config/.config/nvim/lua/plugins/neotest.lua
+++ xdg_config/.config/nvim/lua/plugins/neotest.lua
@@ -1,45 +1,53 @@
 require("neotest").setup({
-	adapters = {
-		require("neotest-python")({
-			-- Extra arguments for nvim-dap configuration
-			-- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-			dap = { justMyCode = false },
-			-- Command line arguments for runner
-			-- Can also be a function to return dynamic values
-			args = { "--log-level", "DEBUG" },
-			-- Runner to use. Will use pytest if available by default.
-			-- Can be a function to return dynamic value.
-			runner = "pytest",
-			-- Custom python path for the runner.
-			-- Can be a string or a list of strings.
-			-- Can also be a function to return dynamic value.
-			-- If not provided, the path will be inferred by checking for
-			-- virtual envs in the local directory and for Pipenev/Poetry configs
-			python = "venv/bin/python",
-			-- -- Returns if a given file path is a test file.
-			-- -- NB: This function is called a lot so don't perform any heavy tasks within it.
-			-- is_test_file = function(file_path)
-			--   return true
-			-- end,
-		}),
-	},
+  adapters = {
+    require("neotest-python")({
+      -- Extra arguments for nvim-dap configuration
+      -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+      dap = { justMyCode = false },
+      -- Command line arguments for runner
+      -- Can also be a function to return dynamic values
+      args = { "--log-level", "DEBUG" },
+      -- Runner to use. Will use pytest if available by default.
+      -- Can be a function to return dynamic value.
+      runner = "pytest",
+      -- Custom python path for the runner.
+      -- Can be a string or a list of strings.
+      -- Can also be a function to return dynamic value.
+      -- If not provided, the path will be inferred by checking for
+      -- virtual envs in the local directory and for Pipenev/Poetry configs
+      python = "venv/bin/python",
+      -- -- Returns if a given file path is a test file.
+      -- -- NB: This function is called a lot so don't perform any heavy tasks within it.
+      -- is_test_file = function(file_path)
+      --   return true
+      -- end,
+    }),
+    require("neotest-plenary"),
+    -- require("neotest-vim-test")({}),
+  },
 })
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>tr",
-	[[<Cmd>lua require("neotest").run.run()<CR>]],
-	{ noremap = true, desc = "[T]est [R]un" }
+  "n",
+  "<leader>tr",
+  [[<Cmd>lua require("neotest").run.run()<CR>]],
+  { noremap = true, desc = "[T]est [R]un" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>to",
-	[[<Cmd>lua require("neotest").output.open()<CR>]],
-	{ noremap = true, desc = "[T]est [O]pen" }
+  "n",
+  "<leader>tf",
+  [[<Cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>]],
+  { noremap = true, desc = "[T]est [F]ile" }
 )
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>ts",
-	[[<Cmd>lua require("neotest").summary.toggle()<CR>]],
-	{ noremap = true, desc = "[T]est [S]ummary" }
+  "n",
+  "<leader>to",
+  [[<Cmd>lua require("neotest").output.open()<CR>]],
+  { noremap = true, desc = "[T]est [O]pen" }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ts",
+  [[<Cmd>lua require("neotest").summary.toggle()<CR>]],
+  { noremap = true, desc = "[T]est [S]ummary" }
 )
