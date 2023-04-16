62baaccf5e081547c46f2c704ca9992d431e906b
Khang Hoang <hoangtrieukhang@gmail.com>
2023-04-16 14:28:04 -0700

    refactor

--- xdg_config/.config/nvim/lua/plugins/aerial_extend.lua
+++ xdg_config/.config/nvim/lua/plugins/aerial_extend.lua
@@ -11,7 +11,16 @@ local function load_ts_tree_from_path(file_path)
     false,
     vim.split(fs_utils.get_file_content(file_path), "\n")
   )
-  local parser = vim.treesitter.get_parser(bufnr, "python")
+
+  vim.api.nvim_buf_set_name(bufnr, file_path)
+
+  -- get the filetype based on the file extension
+  local ext = vim.fn.fnamemodify(file_path, ":e")
+  -- @TODO: need to refactor this to get filetype from buffer's file name
+  local filetype = "python"
+
+  -- @type LanguageTree
+  local parser = vim.treesitter.get_parser(bufnr, filetype)
   return parser:parse()[1], bufnr
 end
 
@@ -19,24 +28,23 @@ M.get_functions_and_classes = function(file_path)
   local tree, bufnr = load_ts_tree_from_path(file_path)
 
   local query_string = [[
-  (function_definition
-    name: (identifier) @name
-    )
-  (class_definition
-    name: (identifier) @name
-    )
-]]
+    (function_definition
+      name: (identifier) @name
+      )
+    (class_definition
+      name: (identifier) @name
+      )
+  ]]
 
+  ---@type Query
   local query = vim.treesitter.query.parse("python", query_string)
 
   -- execute the query on the syntax tree
-  local matches = query:iter_captures(tree:root(), bufnr)
+  local matches = query:iter_captures(tree:root(), bufnr, 0, -1)
 
   local results = {}
 
   for id, node, metadata in matches do
-    local name = query.captures[id] -- name of the capture in the query
-    local q = require("vim.treesitter")
     local node_name_text = vim.treesitter.get_node_text(node, bufnr)
     table.insert(results, node_name_text)
   end
