3ae42d42f12f58b8d636f8e76f5da7bd59c942f5
Khang Hoang <hoangtrieukhang@gmail.com>
2023-04-17 22:22:21 -0700

    more test

--- xdg_config/.config/nvim/lua/plugins/specs/aerial_extend_spec.lua
+++ xdg_config/.config/nvim/lua/plugins/specs/aerial_extend_spec.lua
@@ -1,6 +1,16 @@
 local m = require("plugins.aerial_extend")
+local mock = require("luassert.mock")
+local spy = require("luassert.spy")
 
--- use https://github.com/lunarmodules/luassert
+-- luassert https://github.com/lunarmodules/luassert
+-- busted https://lunarmodules.github.io/busted/#asserts
+
+local function foo(bufnr)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dh", ":lua echo 'foo'", {})
+  return function()
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dh", ":lua echo 'bar'", {})
+  end
+end
 
 describe("aerial_extend", function()
   it("parses function and class names", function()
@@ -16,5 +26,16 @@ describe("aerial_extend", function()
       { "outside_function", "ClassFoo", "inside_function_one", "inside_function_two" },
       results
     )
+  end)
+
+  it("calls vim.keymap", function()
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dh", ":lua echo 'bar'", {})
+
+    local s = spy.on(vim.api, "nvim_buf_set_keymap")
+    local reset = foo(bufnr)
+    assert.spy(s).was.called_with(bufnr, "n", "<leader>dh", ":lua echo 'foo'", {})
+    reset()
+    -- assert.spy(s).was.called_with(bufnr, "n", "<leader>dh", ":lua echo 'foo'", {})
   end)
 end)
