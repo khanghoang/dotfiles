fd4a69d9e0f49e06f1de9de0bb4c07e7e12a19ca
Khang Hoang <hoangtrieukhang@gmail.com>
2023-11-22 00:07:54 -0800

    fix fzf

--- xdg_config/.config/nvim/lua/plugins/fzf.lua
+++ xdg_config/.config/nvim/lua/plugins/fzf.lua
@@ -1,6 +1,7 @@
 return {
+  "junegunn/fzf",
   {
-    "junegunn/fzf",
+    "junegunn/fzf.vim",
     build = function()
       vim.fn["fzf#install"]()
     end,
@@ -21,79 +22,77 @@ return {
       -- api.nvim_set_keymap('n', '<leader>fw', ':Rg!<C-R><C-W><CR>', {noremap = true})
 
       vim.cmd([[
-  command! FZFMru call fzf#run({
-\  'source':  v:oldfiles,
-\  'sink':    'e',
-\  'options': '-m -x +s',
-\  'down':    '40%'})
-]])
-
+          command! FZFMru call fzf#run({
+        \  'source':  v:oldfiles,
+        \  'sink':    'e',
+        \  'options': '-m -x +s',
+        \  'down':    '40%'})
+        ]])
       vim.cmd([[
-function! s:tags_sink(line)
-  let parts = split(a:line, '\t\zs')
-  let excmd = matchstr(parts[2:], '^.*\ze;"\t')
-  execute 'silent e' parts[1][:-2]
-  let [magic, &magic] = [&magic, 0]
-  execute excmd
-  let &magic = magic
-endfunction
+        function! s:tags_sink(line)
+          let parts = split(a:line, '\t\zs')
+          let excmd = matchstr(parts[2:], '^.*\ze;"\t')
+          execute 'silent e' parts[1][:-2]
+          let [magic, &magic] = [&magic, 0]
+          execute excmd
+          let &magic = magic
+        endfunction
 
-function! s:tags()
-  if empty(tagfiles())
-    echohl WarningMsg
-    echom 'Preparing tags'
-    echohl None
-    call system('ctags -R')
-  endif
+        function! s:tags()
+          if empty(tagfiles())
+            echohl WarningMsg
+            echom 'Preparing tags'
+            echohl None
+            call system('ctags -R')
+          endif
 
-  call fzf#run({
-  \ 'source':  'cat '.join(map(tagfiles(), 'fnamemodify(v:val, ":S")')).
-  \            '| grep -v -a ^!',
-  \ 'options': '+m -d "\t" --with-nth 1,4.. -n 1 --tiebreak=index',
-  \ 'down':    '40%',
-  \ 'sink':    function('s:tags_sink')})
-endfunction
+          call fzf#run({
+          \ 'source':  'cat '.join(map(tagfiles(), 'fnamemodify(v:val, ":S")')).
+          \            '| grep -v -a ^!',
+          \ 'options': '+m -d "\t" --with-nth 1,4.. -n 1 --tiebreak=index',
+          \ 'down':    '40%',
+          \ 'sink':    function('s:tags_sink')})
+        endfunction
 
-command! Tags call s:tags()
-]])
-
+        command! Tags call s:tags()
+        ]])
       vim.cmd([[
-function! s:ag_to_qf(line)
-  let parts = split(a:line, ':')
-  return {'filename': parts[0], 'lnum': parts[1], 'col': parts[2],
-        \ 'text': join(parts[3:], ':')}
-endfunction
+      function! s:ag_to_qf(line)
+        let parts = split(a:line, ':')
+        return {'filename': parts[0], 'lnum': parts[1], 'col': parts[2],
+              \ 'text': join(parts[3:], ':')}
+      endfunction
 
-function! s:ag_handler(lines)
-  if len(a:lines) < 2 | return | endif
+      function! s:ag_handler(lines)
+        if len(a:lines) < 2 | return | endif
 
-  let cmd = get({'ctrl-x': 'split',
-               \ 'ctrl-v': 'vertical split',
-               \ 'ctrl-t': 'tabe'}, a:lines[0], 'e')
-  let list = map(a:lines[1:], 's:ag_to_qf(v:val)')
+        let cmd = get({'ctrl-x': 'split',
+                     \ 'ctrl-v': 'vertical split',
+                     \ 'ctrl-t': 'tabe'}, a:lines[0], 'e')
+        let list = map(a:lines[1:], 's:ag_to_qf(v:val)')
 
-  let first = list[0]
-  execute cmd escape(first.filename, ' %#\')
-  execute first.lnum
-  execute 'normal!' first.col.'|zz'
+        let first = list[0]
+        execute cmd escape(first.filename, ' %#\')
+        execute first.lnum
+        execute 'normal!' first.col.'|zz'
 
-  if len(list) > 1
-    call setqflist(list)
-    copen
-    wincmd p
-  endif
-endfunction
+        if len(list) > 1
+          call setqflist(list)
+          copen
+          wincmd p
+        endif
+      endfunction
 
-command! -nargs=* Ag call fzf#run({
-\ 'source':  printf('ag --nogroup --column --color "%s"',
-\                   escape(empty(<q-args>) ? '^(?=.)' : <q-args>, '"\')),
-\ 'sink*':    function('<sid>ag_handler'),
-\ 'options': '--ansi --expect=ctrl-t,ctrl-v,ctrl-x --delimiter : --nth 4.. '.
-\            '--multi --bind=ctrl-a:select-all,ctrl-d:deselect-all '.
-\            '--color hl:68,hl+:110',
-\ 'down':    '50%'
-\ })
-]])
+      command! -nargs=* Ag call fzf#run({
+      \ 'source':  printf('ag --nogroup --column --color "%s"',
+      \                   escape(empty(<q-args>) ? '^(?=.)' : <q-args>, '"\')),
+      \ 'sink*':    function('<sid>ag_handler'),
+      \ 'options': '--ansi --expect=ctrl-t,ctrl-v,ctrl-x --delimiter : --nth 4.. '.
+      \            '--multi --bind=ctrl-a:select-all,ctrl-d:deselect-all '.
+      \            '--color hl:68,hl+:110',
+      \ 'down':    '50%'
+      \ })
+      ]])
 
       -- api.nvim_set_keymap('n', '<leader>p', '<leader>ff', {noremap = false})
       -- api.nvim_set_keymap('n', '<leader>s', '<leader>fg', {noremap = false})
@@ -176,6 +175,25 @@ command! -nargs=* Ag call fzf#run({
           },
         },
       })
+
+      vim.keymap.set(
+        "n",
+        "gp",
+        "<cmd>FzfLua lsp_live_workspace_symbols<CR>",
+        { noremap = true, silent = true, desc = "Search all symbols" }
+      )
+      vim.keymap.set(
+        "n",
+        "lg",
+        "<cmd>FzfLua live_grep<CR>",
+        { noremap = true, silent = true, desc = "Grep search" }
+      )
+      vim.keymap.set(
+        "n",
+        "<C-p>",
+        "<cmd>FzfLua keymaps<CR>",
+        { noremap = true, silent = true, desc = "Search keymaps" }
+      )
     end,
   },
 }
--- xdg_config/.config/nvim/lua/plugins/nvim_cmp.lua
+++ xdg_config/.config/nvim/lua/plugins/nvim_cmp.lua
@@ -1,80 +1,81 @@
 return {
-  "hrsh7th/nvim-cmp",
-  config = function()
-    local cmp = require("cmp")
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          -- For `vsnip` user.
-          -- vim.fn["vsnip#anonymous"](args.body)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<Tab>"] = function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
-        end,
-      }),
-      sources = {
-        -- Copilot Source
-        { name = "copilot", group_index = 2 },
-        -- other sources
-        { name = "nvim_lsp_signature_help" },
-        { name = "luasnip" },
-        { name = "nvim_lsp" },
-        -- For vsnip user.
-        { name = "path" },
-        { name = "vsnip" },
-        { name = "buffer" },
-      },
-      formatting = {
-        format = function(entry, vim_item)
-          local LSP_KIND_ICONS = {
-            Text = "  ",
-            Method = "  ",
-            Function = "  ",
-            Constructor = "  ",
-            Field = "  ",
-            Variable = "  ",
-            Class = "  ",
-            Interface = "  ",
-            Module = "  ",
-            Property = "  ",
-            Unit = "  ",
-            Value = "  ",
-            Enum = "  ",
-            Keyword = "  ",
-            Snippet = "  ",
-            Color = "  ",
-            File = "  ",
-            Reference = "  ",
-            Folder = "  ",
-            EnumMember = "  ",
-            Constant = "  ",
-            Struct = "  ",
-            Event = "  ",
-            Operator = "  ",
-            TypeParameter = "  ",
-            Copilot = "",
-          }
-          vim_item.kind = string.format("%s %s", LSP_KIND_ICONS[vim_item.kind], vim_item.kind)
-          return vim_item
-        end,
-      },
-    })
-  end,
-  dependencies = { "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp", "neovim/nvim-lspconfig" },
-},
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            -- For `vsnip` user.
+            -- vim.fn["vsnip#anonymous"](args.body)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end,
+        }),
+        sources = {
+          -- Copilot Source
+          { name = "copilot", group_index = 2 },
+          -- other sources
+          { name = "nvim_lsp_signature_help" },
+          { name = "luasnip" },
+          { name = "nvim_lsp" },
+          -- For vsnip user.
+          { name = "path" },
+          { name = "vsnip" },
+          { name = "buffer" },
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            local LSP_KIND_ICONS = {
+              Text = "  ",
+              Method = "  ",
+              Function = "  ",
+              Constructor = "  ",
+              Field = "  ",
+              Variable = "  ",
+              Class = "  ",
+              Interface = "  ",
+              Module = "  ",
+              Property = "  ",
+              Unit = "  ",
+              Value = "  ",
+              Enum = "  ",
+              Keyword = "  ",
+              Snippet = "  ",
+              Color = "  ",
+              File = "  ",
+              Reference = "  ",
+              Folder = "  ",
+              EnumMember = "  ",
+              Constant = "  ",
+              Struct = "  ",
+              Event = "  ",
+              Operator = "  ",
+              TypeParameter = "  ",
+              Copilot = "",
+            }
+            vim_item.kind = string.format("%s %s", LSP_KIND_ICONS[vim_item.kind], vim_item.kind)
+            return vim_item
+          end,
+        },
+      })
+    end,
+    dependencies = { "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp", "neovim/nvim-lspconfig" },
+  },
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-vsnip",
   "hrsh7th/vim-vsnip",
@@ -112,4 +113,5 @@ return {
         end
       end)
     end,
-  }
+  },
+}
