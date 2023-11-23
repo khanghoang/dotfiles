cbe66270af7d0acac8d0bd866cc1567d7ccf01c2
Khang Hoang <hoangtrieukhang@gmail.com>
2023-11-22 18:17:21 -0800

    fix keymaps

--- xdg_config/.config/nvim/lua/plugins/treesitter.lua
+++ xdg_config/.config/nvim/lua/plugins/treesitter.lua
@@ -59,11 +59,11 @@ return {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-              ["]f"] = "@function.outer",
+              -- ["]m"] = "@function.outer",
               ["]]"] = "@class.outer",
             },
             goto_next_end = {
-              ["]f"] = "@function.outer",
+              ["]m"] = "@function.outer",
               ["]["] = "@class.outer",
             },
             goto_previous_start = {
@@ -71,7 +71,7 @@ return {
               ["[["] = "@class.outer",
             },
             goto_previous_end = {
-              ["[M"] = "@function.outer",
+              -- ["]m"] = "@function.outer",
               ["[]"] = "@class.outer",
             },
           },
